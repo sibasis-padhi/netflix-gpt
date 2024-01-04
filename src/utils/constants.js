c83968f00acfb75f3005323eb1da0d1061a7509c
Sibasis Padhi <sibasis.padhi@outlook.com>
2024-01-05 00:10:03 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,11 @@ export const BG_URL =
 
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+  },
+};
