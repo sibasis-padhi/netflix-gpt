c54fea36879971d84bd8513907dccd528ccb5799
Sibasis Padhi <sibasis.padhi@outlook.com>
2024-01-08 23:22:49 +0530

    Update Header.js

--- src/components/UI/Navigation/Header.js
+++ src/components/UI/Navigation/Header.js
@@ -51,14 +51,11 @@ const Header = () => {
       {user && (
         <div className="dropdown inline-block relative">
           <img
-            className="md:block w-12 h-12 cursor-pointer"
+            className="md:block w-12 h-12 cursor-pointer rounded-lg"
             alt="usericon"
             src={user?.photoURL}
           />
           <ul class="dropdown-menu absolute hidden text-gray-700 pt-1 w-32 -ml-10">
-            <li className="bg-gray-200 hover:bg-gray-400 py-2 px-1 block whitespace-no-wrap font-bold text-black text-center cursor-pointer">
-              Search
-            </li>
             <li
               className="bg-gray-200 hover:bg-gray-400 py-2 px-1 block whitespace-no-wrap font-bold text-black text-center cursor-pointer"
               onClick={handleSignOut}
