08a01aefe923d6931946593a4ed8bf2d5ca00d06
Sibasis Padhi <sibasis.padhi@outlook.com>
2024-01-10 02:24:18 +0530

    toggle option between gpt & normal window

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,17 +1,26 @@
+import { useSelector } from "react-redux";
 import useNowPlayingMovies from "../../hooks/useNowPlayingMovies";
 import usePopularMovies from "../../hooks/usePopularMovies";
 import Header from "../UI/Navigation/Header";
 import Banner from "./Banner";
 import Body from "./Body";
+import GptSearch from "../GPT/GptSearch";
 
 const HomePage = () => {
+  const showGptSearchView = useSelector((state) => state.gpt.showGptSearch);
   useNowPlayingMovies();
   usePopularMovies();
   return (
     <>
       <Header />
-      <Banner />
-      <Body />
+      {showGptSearchView ? (
+        <GptSearch />
+      ) : (
+        <>
+          <Banner />
+          <Body />
+        </>
+      )}
     </>
   );
 };
